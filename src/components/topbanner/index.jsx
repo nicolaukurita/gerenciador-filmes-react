a10294557c5752613d9c72bd9a12bf6b53e69a62
nicolau <nicolau@etematica.com.br>
2025-10-20 13:11:21 -0300

    Adição de logo responsivo

--- src/components/topbanner/index.jsx
+++ src/components/topbanner/index.jsx
@@ -1,8 +1,10 @@
 import "./style.css"
+import Logo from  "./imgs/logo.png"
 
 export function Topbanner() {  
     return (
         <div className="topbanner">
+            <img src={Logo} alt="Logo" className="topbanner"/>
             <center className="topbanner">Gerenciador de Filmes</center>
         </div>
     );
