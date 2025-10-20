76b858dfc12cf3e31031988b31ea0d12208d936d
nicolau <nicolau@etematica.com.br>
2025-10-20 13:57:20 -0300

    Criação do footer

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,9 @@
+import "./style.css"
+
+export function Footer() {  
+    return (
+        <div className="mainfooter">
+            <div className="footer"> footer</div>
+        </div>
+    );
+}
