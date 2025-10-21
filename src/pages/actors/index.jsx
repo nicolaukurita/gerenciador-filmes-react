0f70aa46297ccc3e0616619db790413cb52d22b8
nicolau <nicolau@etematica.com.br>
2025-10-21 15:42:25 -0300

    Configuração da variavel de ambiente

--- src/pages/actors/index.jsx
+++ src/pages/actors/index.jsx
@@ -1,4 +1,7 @@
+import { BACKEND_URL } from "../../config";
+
 export function Actors() {
+    console.log("API: " + BACKEND_URL);
     return (
         <div className="content">
             <h1>Atores</h1>
