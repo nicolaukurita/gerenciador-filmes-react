40c2f98b4cb133ec3c85869340e4d2376d1328c2
nicolau <nicolau@etematica.com.br>
2025-10-21 23:13:53 -0300

    Buscando atores do backend

--- src/pages/actors/index.jsx
+++ src/pages/actors/index.jsx
@@ -1,11 +1,39 @@
+import "./style.css";
+import axios from "axios";
 import { BACKEND_URL } from "../../config";
+import { useEffect, useState } from "react";
+
+function actorsHandler(data) {
+    return data.map((actor) => (
+        <div key={actor.id} className="actors-content">
+            {actor.name}</div>
+    ));
+}
 
 export function Actors() {
-    console.log("API: " + BACKEND_URL);
+
+    const [actors, setActors] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${BACKEND_URL}/actors`)
+            .then((response) => {
+                console.log(response.data);
+                setActors(actorsHandler(response.data));
+            })
+            .catch((error) => {
+                console.error("There was an error fetching the actors!", error);
+            }).finally(() => {
+                console.log("Request completed");
+            });
+    }, []);
+
+    useState();
+
     return (
         <div className="content">
             <h1>Atores</h1>
-            <div>Conteudo</div>
+            {actors}
+            <p><button onClick={() => setActors("cliquei no botao")}>Clique aqui!</button></p>
         </div>
     );
 }
