0a357738ddea81ec3f7ce44e6f538f27089bb5f7
nicolau <nicolau@etematica.com.br>
2025-10-22 04:20:58 -0300

    Inicio do cadastro de filmes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,7 +4,15 @@ export function Home() {
     return (
         <div className="content">
             <h1>Home</h1>
-            <div>Conteudo</div>
+            <div className="home-form">
+                <h3 className="home-form">Inserir novo filme:</h3>
+                <p className="home-form"><input className="home-form" type="text" size={40} placeholder="Nome do Filme"/></p>
+                <p className="home-form"><input className="home-form" type="text" size={40} placeholder="Diretor"/></p>
+                <p className="home-form"><input className="home-form" type="text" size={10} placeholder="Nota"/></p>
+
+                <h3 className="home-form">Elenco:</h3>
+                <p className="home-form"><input className="home-form" type="text" size={40} placeholder="Nome do ator"/></p>
+            </div>
         </div>
     );
 }
