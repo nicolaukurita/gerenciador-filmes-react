47468a4436f60320cb5017be9918ac02ef55b51d
nicolau <nicolau@etematica.com.br>
2025-10-23 20:01:11 -0300

    Adicionando nome aos campos

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -16,7 +16,7 @@ function createcastInputs(castSize, setCastSize) {
         content.push(
             (
                 <p className="home-form" key={i}>
-                    <input className="home-form" type="text" size={40} placeholder="Nome do ator" />
+                    <input className="home-form" type="text" size={40} name="actor" placeholder="Nome do ator" />
                     <MinusIcon size={25} className="home-form-icon" onClick={() => decreaseCastSize(castSize, setCastSize)} />
                     <PlusIcon size={25} className="home-form-icon" onClick={() => setCastSize(castSize + 1)} />
                 </p>
@@ -24,6 +24,22 @@ function createcastInputs(castSize, setCastSize) {
         )
     }
     return content;
+}
+
+function formHandle(event) {
+    event.preventDefault();
+
+    const film = event.target.film.value;
+    const director = event.target.director.value;
+    const grade = event.target.grade.value;
+
+    const actors = Array.from(event.target.actor).map(actor => actor.value);
+
+    console.log({ film, director, grade, actors });
+    console.log(actors);
+    console.log(director);
+    console.log(film);
+    console.log(grade);
 }
 
 export function Home() {
@@ -34,16 +50,18 @@ export function Home() {
         <div className="content">
             <h1>Home</h1>
             <div className="home-form">
-                <h3 className="home-form">Inserir novo filme:</h3>
-                <p className="home-form"><input className="home-form" type="text" size={40} placeholder="Nome do Filme" /></p>
-                <p className="home-form"><input className="home-form" type="text" size={40} placeholder="Diretor" /></p>
-                <p className="home-form"><input className="home-form" type="text" size={10} placeholder="Nota" /></p>
+                <form onSubmit={formHandle}>
+                    <h3 className="home-form">Inserir novo filme:</h3>
+                    <p className="home-form"><input className="home-form" type="text" size={40} name="film" placeholder="Nome do Filme" /></p>
+                    <p className="home-form"><input className="home-form" type="text" size={40} name="director" placeholder="Diretor" /></p>
+                    <p className="home-form"><input className="home-form" type="text" size={10} name="grade" placeholder="Nota" /></p>
 
-                <h3 className="home-form">Elenco:</h3>
-                
-                {createcastInputs(castSize, setCastSize)}
-                
-                <p className="home-form"><input type="submit" className="home-form-button" value="Cadastrar novo filme" /></p>
+                    <h3 className="home-form">Elenco:</h3>
+
+                    {createcastInputs(castSize, setCastSize)}
+
+                    <p className="home-form"><input type="submit" className="home-form-button" value="Cadastrar novo filme" /></p>
+                </form>
             </div>
         </div>
     );
